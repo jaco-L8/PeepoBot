efe395b24ea3a525828a2887662beee1b7547f7e
jaco-L8 <alajmany1@gmail.com>
2024-01-10 14:38:54 +0400

    removed lightLive

--- events/LightLive.js
+++ events/LightLive.js
@@ -1,10 +1,5 @@
-const { ApiClient } = require('twitch');
-const { ClientCredentialsAuthProvider } = require('twitch-auth');
 const fs = require('fs');
 const config = require('../config.json');
-const { getUserAccessToken } = require("twitch-auth");
-
-let isLive = false;
 
 module.exports = {
     name: 'ready',
@@ -14,24 +9,5 @@ module.exports = {
             console.warn(`Channel with ID 'channel-id-here' not found.`);
             return;
         }
-
-        const clientId = config.twitchId;
-        const clientSecret = config.twitchSecret;
-        const refreshToken = config.twitchRefreshToken;
-
-        const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
-        const apiClient = new ApiClient({ authProvider });
-
-        setInterval(async () => {
-            const userId = '537554067'; // Light's user ID
-            const streams = await apiClient.helix.streams.getStreams({ userId: userId });
-            if (streams.data.length > 0 && !isLive) {
-                channel.send(`light it currently live! \n\n https://www.twitch.tv/thelightthel`);
-                console.log('light is live!');
-                isLive = true;
-            } else if (streams.data.length === 0 && isLive) {
-                isLive = false;
-            }
-        }, 1000); // Check every minute
     },
 };
